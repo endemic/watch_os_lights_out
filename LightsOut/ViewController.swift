88d2e50052aa564bc4d8d6f8a35942b52f4c4cb8
Nathan Demick <ndemick@gmail.com>
2016-04-28 14:53:00 -0400

    Initial Commit

---
+++ LightsOut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LightsOut
+//
+//  Created by Demick Family on 4/28/16.
+//  Copyright © 2016 Ganbaru Games. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
